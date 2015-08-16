e1e6552650832e054f52497562d8aa37612d4065
John F. Lewis <johnflewis93@gmail.com>
2015-08-16 17:08:19 +0100

    migrate to wfLoadExtension

--- MirahezeMagic.php
+++ MirahezeMagic.php
@@ -1,18 +1,7 @@
 <?php
-if ( !defined( 'MEDIAWIKI' ) ) {
-        exit( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'MirahezeMagic' );
+	return;
+} else {
+	die( 'This version requires MediaWiki 1.25+' );
 }
-$wgExtensionCredits['other'][] = array(
-        'author'         => 'John Lewis',
-        'descriptionmsg' => 'mirahezemagic-description',
-        'name'           => 'MirahezeMagic',
-        'path'           => __FILE__,
-        'url'            => '//github.com/Miraheze/MirahezeMagic',
-);
-
-$wgMessagesDirs['MirahezeMagic'] = dirname( __FILE__ ) . '/i18n/miraheze';
-$wgMessagesDirs['MirahezeOverrideMessagesMagic'] = dirname( __FILE__ ) . '/i18n/overrides';
-
-$wgAutoloadClasses['MirahezeMagicHooks'] = __DIR__ . '/MirahezeMagic.hooks.php';
-
-$wgHooks['MessageCache::get'][] = 'MirahezeMagicHooks::onMessageCacheGet';
