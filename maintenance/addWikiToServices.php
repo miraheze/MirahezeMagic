bb07b6f0d27c7dab3262f3e69c8602b56d5f6c86
paladox <paladox@users.noreply.github.com>
2018-08-01 22:16:01 +0100

    Update script

--- maintenance/addWikiToServices.php
+++ maintenance/addWikiToServices.php
@@ -10,42 +10,36 @@ class addWikiToServices extends Maintenance {
 	public function execute() {
 		global $wgServicesRepo;
 
-		/*$dbw = wfGetDB( DB_MASTER );
-
-		$res = $dbw->select(
-			'cw_wikis',
-			'*',
-			array(),
-			__METHOD__
-		);
-
-		if ( !$res || !is_object( $res ) ) {
-			throw new MWException( '$res was not set to a valid array.' );
-		}*/
-
 		$allWikis = array();
 
 		if ( file_exists("$wgServicesRepo/services.yaml") ) {
-			//foreach ( explode("\n", file_get_contents('/srv/mediawiki/dblist/all.dblist')) as $wiki ) {
-			        $wikis = file( '/srv/mediawiki/dblist/all.dblist' );
-				foreach ( $wikis as $wiki ) {
-					$wiki = explode( '|', $wiki);
-				        $DBname = $wiki[0];
-				        $remote = RemoteWiki::newFromName( $wiki[0] )->getSettingsValue( 'wgServer' );
-				        $flow = RemoteWiki::newFromName( $wiki[0] )->hasExtension( 'flow' );
-				        $visualEditor = RemoteWiki::newFromName( $wiki[0] )->hasExtension( 'visualeditor' );
-				        $this->output($flow);
-				        $this->output($visualEditor);
-				        if ( $visualEditor || $flow ) {
-					        $custom_domain = $remote ? str_replace('https://', '', "'" . $remote . "'") : 'true';
+			$wikis = file( '/srv/mediawiki/dblist/all.dblist' );
+			foreach ( $wikis as $wiki ) {
+				$wiki = explode( '|', $wiki);
+					$DBname = $wiki[0];
+					$remote = RemoteWiki::newFromName( $wiki[0] )->getSettingsValue( 'wgServer' );
+					$flow = RemoteWiki::newFromName( $wiki[0] )->hasExtension( 'flow' );
+				$visualEditor = RemoteWiki::newFromName( $wiki[0] )->hasExtension( 'visualeditor' );
+				if ( !is_null( $wiki[3] ) ) {
+					$visualEditor = $this->hasExt( 'visualeditor', $wiki[3] );
+					$flow = $this->hasExt( 'flow', $wiki[3] );
+					if ( $visualEditor || $flow ) {
+						$custom_domain = $remote ? str_replace('https://', '', "'" . $remote . "'") : 'true';
 
-					        $allWikis[] = "$DBname: $custom_domain";
-				        }
+							$allWikis[] = "$DBname: $custom_domain";
+						}
+				}
 				
 			}
 
 			file_put_contents( "$wgServicesRepo/services.yaml", implode( "\n", $allWikis ), LOCK_EX );
 		}
+	}
+
+	public function hasExt( $ext, $listExt ) {
+		$extensionsarray = explode( ",", $listExt );
+
+		return in_array( $ext, $extensionsarray );
 	}
 }
 
