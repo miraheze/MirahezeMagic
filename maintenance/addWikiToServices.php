a45b9b11129d2943158abae25c4194f3e1d89902
paladox <paladox@users.noreply.github.com>
2018-08-01 21:25:39 +0100

    Update script

--- maintenance/addWikiToServices.php
+++ maintenance/addWikiToServices.php
@@ -10,7 +10,7 @@ class addWikiToServices extends Maintenance {
 	public function execute() {
 		global $wgServicesRepo;
 
-		$dbw = wfGetDB( DB_MASTER );
+		/*$dbw = wfGetDB( DB_MASTER );
 
 		$res = $dbw->select(
 			'cw_wikis',
@@ -21,21 +21,27 @@ class addWikiToServices extends Maintenance {
 
 		if ( !$res || !is_object( $res ) ) {
 			throw new MWException( '$res was not set to a valid array.' );
-		}
+		}*/
 
 		$allWikis = array();
 
 		if ( file_exists("$wgServicesRepo/services.yaml") ) {
-			foreach ( $res as $row ) {
-				$DBname = $row->wiki_dbname;
-				$remote = RemoteWiki::newFromName( $DBname )->getSettingsValue( 'wgServer' );
-				$flow = RemoteWiki::newFromName( $DBname )->hasExtension( 'flow' );
-				$visualEditor = RemoteWiki::newFromName( $DBname )->hasExtension( 'visualeditor' );
-				if ( $visualEditor || $flow ) {
-					$custom_domain = $remote ? str_replace('https://', '', "'" . $remote . "'") : 'true';
+			//foreach ( explode("\n", file_get_contents('/srv/mediawiki/dblist/all.dblist')) as $wiki ) {
+			        $wikis = file( '/srv/mediawiki/dblist/all.dblist' );
+				foreach ( $wikis as $wiki ) {
+					$wiki = explode( '|', $wiki);
+				        $DBname = $wiki[0];
+				        $remote = RemoteWiki::newFromName( $wiki[0] )->getSettingsValue( 'wgServer' );
+				        $flow = RemoteWiki::newFromName( $wiki[0] )->hasExtension( 'flow' );
+				        $visualEditor = RemoteWiki::newFromName( $wiki[0] )->hasExtension( 'visualeditor' );
+				        $this->output($flow);
+				        $this->output($visualEditor);
+				        if ( $visualEditor || $flow ) {
+					        $custom_domain = $remote ? str_replace('https://', '', "'" . $remote . "'") : 'true';
 
-					$allWikis[] = "$DBname: $custom_domain";
-				}
+					        $allWikis[] = "$DBname: $custom_domain";
+				        }
+				
 			}
 
 			file_put_contents( "$wgServicesRepo/services.yaml", implode( "\n", $allWikis ), LOCK_EX );
