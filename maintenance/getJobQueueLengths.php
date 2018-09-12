359a0dae4a57a2b5d1052110ba2d005f398d2e98
paladox <paladox@users.noreply.github.com>
2018-09-12 20:00:41 +0100

    Update getJobQueueLengths to match upstream changes

--- maintenance/getJobQueueLengths.php
+++ maintenance/getJobQueueLengths.php
@@ -1,9 +1,14 @@
 <?php
+
 /**
- * Get the length of the job queue on all wikis
+ * Get the length of the job queue on all wikis in $wgConf
  * Sourced from WikimediaMaintenance
  */
+
 require_once( '/srv/mediawiki/w/maintenance/Maintenance.php' );
+
+use MediaWiki\MediaWikiServices;
+
 class GetJobQueueLengths extends Maintenance {
 	function __construct() {
 		parent::__construct();
@@ -11,19 +16,40 @@ class GetJobQueueLengths extends Maintenance {
 		$this->addOption( 'totalonly', 'Whether to only output the total number of jobs' );
 		$this->addOption( 'nototal', "Don't print the total number of jobs" );
 		$this->addOption( 'grouponly', "Show a per-wiki/per-type count map in JSON" );
+		$this->addOption( 'report', "Report the total count of job queue items to StatsD" );
 	}
+
 	function execute() {
+		if ( $this->hasOption( 'grouponly' ) && $this->hasOption( 'report' ) ) {
+			$this->fatalError( "You cannot specify both '--report' and '--grouponly'." );
+		}
+
 		$totalOnly = $this->hasOption( 'totalonly' );
+
 		$pendingDBs = JobQueueAggregator::singleton()->getAllReadyWikiQueues();
-		$sizeByWiki = array(); // (wiki => type => count) map
+		$sizeByWiki = []; // (wiki => type => count) map
 		foreach ( $pendingDBs as $type => $wikis ) {
 			foreach ( $wikis as $wiki ) {
 				$sizeByWiki[$wiki][$type] =
 					JobQueueGroup::singleton( $wiki )->get( $type )->getSize();
 			}
 		}
+
+		uasort(
+			$sizeByWiki,
+			function ( $typeTotalsA, $typeTotalsB ) {
+				$sumA = array_sum( $typeTotalsA );
+				$sumB = array_sum( $typeTotalsB );
+				if ( $sumA == $sumB ) {
+					return 0;
+				}
+
+				return ( $sumA > $sumB ) ? -1 : 1;
+			}
+		);
+
 		if ( $this->hasOption( 'grouponly' ) ) {
-			$this->output( FormatJSON::encode( $sizeByWiki, true ) . "\n" );
+			$this->output( FormatJson::encode( $sizeByWiki, true ) . "\n" );
 		} else {
 			$total = 0;
 			foreach ( $sizeByWiki as $wiki => $counts ) {
@@ -38,8 +64,14 @@ class GetJobQueueLengths extends Maintenance {
 			if ( !$this->hasOption( 'nototal' ) ) {
 				$this->output( "Total $total\n" );
 			}
+
+			if ( $this->hasOption( 'report' ) ) {
+				MediaWikiServices::getInstance()->getStatsdDataFactory()
+					->gauge( 'jobqueue.size', $total );
+			}
 		}
 	}
 }
+
 $maintClass = 'GetJobQueueLengths';
-require_once( DO_MAINTENANCE );
+require_once RUN_MAINTENANCE_IF_MAIN;
