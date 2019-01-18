a7360f4c59d8fc3497a6ba5a37efc217893e8cfb
paladox <paladox@users.noreply.github.com>
2019-01-18 16:04:45 +0000

    Add InsertNamespaces script
    This is to insert the missing main namespaces

---
+++ maintenance/insertNamespaces.php
@@ -0,0 +1,81 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+class InsertNamespaces extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	function execute() {
+		if ( ManageWiki::checkSetup( 'namespaces' ) ) {
+			$this->fatalError( 'Disable ManageWiki Namespaces on this wiki.' );
+		}
+
+		$dbw = wfGetDB( DB_MASTER, [], $wgCreateWikiDatabase );
+
+		$res = $dbw->select(
+			'mw_namespaces',
+			[
+				'*',
+			],
+			[
+				'ns_dbname' => 'default',
+			],
+			__METHOD__
+		);
+
+
+
+		$this->insertNamespace( $dbw, $res);
+	}
+	
+	public function insertNamespace( $dbw, $res ) {
+		global $wgDBname, $wgNamespacesToBeSearchedDefault, $wgNamespacesWithSubpages,
+			$wgContentNamespaces, $wgNamespaceProtection;
+
+		$resObj = $dbw->select(
+			'mw_namespaces',
+			[
+				'*',
+			],
+			[
+				'ns_dbname' => $wgDBname,
+				'ns_namespace_id' => $res->ns_namespace_id,
+				'ns_namespace_name' => $res->ns_namespace_name,
+				'ns_searchable' => $res->ns_searchable,
+				'ns_subpages' => $res->ns_subpages,
+				'ns_content' => $res->ns_content,
+				'ns_protection' => $res->ns_protection,
+				'ns_aliases' => $res->ns_aliases,
+				'ns_core' => $res->ns_core,
+			],
+			__METHOD__
+		);
+
+		if ( !$resObj || !is_object( $resObj ) ) {
+			$dbw->insert(
+				'mw_namespaces',
+				[
+					'ns_dbname' => $wgDBname,
+					'ns_namespace_id' => $res->ns_namespace_id,
+					'ns_namespace_name' => $res->ns_namespace_name,
+					'ns_searchable' => $res->ns_searchable,
+					'ns_subpages' => $res->ns_subpages,
+					'ns_content' => $res->ns_content,
+					'ns_protection' => $res->ns_protection,
+					'ns_aliases' => $res->ns_aliases,
+					'ns_core' => $res->ns_core,
+				],
+				__METHOD__
+			);
+		}
+	}
+}
+
+$maintClass = 'InsertNamespaces';
+require_once RUN_MAINTENANCE_IF_MAIN;
