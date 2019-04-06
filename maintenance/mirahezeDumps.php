c1bfca7e964587957e7675b0f18cec3ba1509f1e
paladox <paladox@users.noreply.github.com>
2019-04-06 02:02:24 +0100

    Add mirahezeDumps script

---
+++ maintenance/mirahezeDumps.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../../../maintenance/Maintenance.php';
+
+class mirahezeDumps extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		
+		$this->addOption( 'file-name', 'The name of the file to dump to.', false, false );
+		$this->addOption( 'use-gz', 'Use gzip to compress file', false, false );
+	}
+
+	public function execute() {
+		global $wgServicesRepo;
+
+        $fileName = wfEscapeShellArg( $this->getOption( 'file-name' ) );
+
+		exec(
+			"/usr/bin/php /srv/mediawiki/w/maintenance/dumpBackup.php" . $this->getOption( 'wiki' ),
+			"--full > ${fileName}"
+		);
+
+        exec( "gzip -c ${$fileName}.gz > ${fileName}" );
+	}
+}
+
+$maintClass = 'mirahezeDumps';
+require_once RUN_MAINTENANCE_IF_MAIN;
