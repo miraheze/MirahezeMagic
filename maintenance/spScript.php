89354b9eadac8865e985939e0da9bbd017073ef1
paladox <paladox@users.noreply.github.com>
2020-04-07 02:16:22 +0100

    Update spScript.php

--- maintenance/spScript.php
+++ maintenance/spScript.php
@@ -10,106 +10,106 @@ class spScript extends Maintenance {
 	public function execute() {
 		global $wgCreateWikiDatabase, $wgDBname;
 		
-		$up = $this->getSettingsValue( 'wgUserProfileDisplay', $wgDBname );
-		
-		if ( isset( $up ) && $up ) {
-			$dbw = wfGetDB( DB_MASTER, [], $wgCreateWikiDatabase );
-			
-			if ( !isset( $up->activity ) ) {
-				$up['activity'] = false;
+		$dbw = wfGetDB( DB_MASTER, [], $wgCreateWikiDatabase );
+
+		$res = $dbw->select(
+			'cw_wikis',
+			[
+				'wiki_dbname',
+				'wiki_settings',
+			],
+			[
+				'wiki_dbame' => $wgDBname
+			],
+		);
+
+		foreach ( $res as $row ) {
+			$settings = json_decode( $row->wiki_settings, true );
+
+			if ( $settings && isset( $settings['wgUserProfileDisplay'] ) && $settings['wgUserProfileDisplay'] ) {
+				if ( !isset( $settings['wgUserProfileDisplay']['activity'] ) ) {
+					$settings['wgUserProfileDisplay']['activity'] = false;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['articles'] ) ) {
+					$settings['wgUserProfileDisplay']['articles'] = true;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['avatar'] ) ) {
+					$settings['wgUserProfileDisplay']['avatar'] = true;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['awards'] ) ) {
+					$settings['wgUserProfileDisplay']['awards'] = true;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['board'] ) ) {
+					$settings['wgUserProfileDisplay']['board'] = false;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['custom'] ) ) {
+					$settings['wgUserProfileDisplay']['custom'] = true;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['foes'] ) ) {
+					$settings['wgUserProfileDisplay']['foes'] = false;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['friends'] ) ) {
+					$settings['wgUserProfileDisplay']['friends'] = false;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['games'] ) ) {
+					$settings['wgUserProfileDisplay']['games'] = false;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['gifts'] ) ) {
+					$settings['wgUserProfileDisplay']['gifts'] = true;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['interests'] ) ) {
+					$settings['wgUserProfileDisplay']['interests'] = true;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['personal'] ) ) {
+					$settings['wgUserProfileDisplay']['personal'] = true;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['profile'] ) ) {
+					$settings['wgUserProfileDisplay']['profile'] = true;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['stats'] ) ) {
+					$settings['wgUserProfileDisplay']['stats'] = false;
+				}
+
+				if ( !isset( $settings['wgUserProfileDisplay']['userboxes'] ) ) {
+					$settings['wgUserProfileDisplay']['userboxes'] = false;
+				}
+
+				$dbw->update(
+					'cw_wikis',
+					[
+						'wiki_settings' => json_encode( $settings ),
+					],
+					[
+						'wiki_dbname' => $wgDBname
+					],
+					__METHOD__
+				);
+
+				$dbw->update(
+					'mw_settings',
+					[
+						's_settings' => json_encode( $settings ),
+					],
+					[
+						's_dbname' => $wgDBname
+					],
+					__METHOD__
+				);
 			}
-
-			if ( !isset( $up->articles ) ) {
-				$up['articles'] = true;
-			}
-
-			if ( !isset( $up->avatar ) ) {
-				$up['avatar'] = true;
-			}
-
-			if ( !isset( $up->awards ) ) {
-				$up['awards'] = true;
-			}
-
-			if ( !isset( $up->board ) ) {
-				$up['board'] = false;
-			}
-
-			if ( !isset( $up->custom ) ) {
-				$up['custom'] = true;
-			}
-
-			if ( !isset( $up->foes ) ) {
-				$up['foes'] = false;
-			}
-
-			if ( !isset( $up->friends ) ) {
-				$up['friends'] = false;
-			}
-
-			if ( !isset( $up->games ) ) {
-				$up['games'] = false;
-			}
-
-			if ( !isset( $up->gifts ) ) {
-				$up['gifts'] = true;
-			}
-
-			if ( !isset( $up->interests ) ) {
-				$up['interests'] = true;
-			}
-
-			if ( !isset( $up->personal ) ) {
-				$up['personal'] = true;
-			}
-
-			if ( !isset( $up->profile ) ) {
-				$up['profile'] = true;
-			}
-
-			if ( !isset( $up->stats ) ) {
-				$up['stats'] = false;
-			}
-
-			if ( !isset( $up->userboxes ) ) {
-				$up['userboxes'] = false;
-			}
-
-			$dbw->update(
-				'cw_wikis',
-				[
-					'wiki_settings' => json_encode( $up ),
-				],
-				[
-					'wiki_dbname' => $wgDBname
-				],
-				__METHOD__
-			);
-
-			$dbw->update(
-				'mw_settings',
-				[
-					's_settings' => json_encode( $up ),
-				],
-				[
-					's_dbname' => $wgDBname
-				],
-				__METHOD__
-			);
 		}
-	}
-
-	/**
-	 * Similar to CreateWiki RemoteWiki function, just this reads from a file.
-	 */
-	private function getSettingsValue( $setting, $settingsjson ) {
-		$settingsarray = json_decode( $settingsjson, true );
-
-		if ( isset( $settingsarray[$setting] ) ) {
-			return $settingsarray[$setting];
-		}
-
-		return null;
 	}
 }
 
