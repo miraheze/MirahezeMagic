a68124e0901db59db5a5985f4c9f5e5f462462e9
paladox <paladox@users.noreply.github.com>
2020-04-23 00:09:46 +0100

    Update lowercaseGroups.php

--- maintenance/lowercaseGroups.php
+++ maintenance/lowercaseGroups.php
@@ -8,39 +8,62 @@ class lowercaseGroups extends Maintenance {
 	}
 
 	public function execute() {
-		global $wgCreateWikiDatabase, $wgServicesRepo;
+		global $wgCreateWikiDatabase;
 
-        $dbw = wfGetDB( DB_MASTER );
+		$dbw = wfGetDB( DB_MASTER, [], $wgCreateWikiDatabase );
+		
+		$dbw_wiki = wfGetDB( DB_MASTER );
 
-        $res = $dbw->select(
-            'mw_permissions',
-            [
-                'perm_dbanme',
-                'perm_group',
-            ],
-            [],
-            __METHOD__
-        );
+		$res = $dbw->select(
+		    'mw_permissions',
+		    [
+			'perm_dbanme',
+			'perm_group',
+		    ],
+		    [],
+		    __METHOD__
+		);
 
-        if ( !$res || !is_object( $res ) ) {
-            throw new MWException( '$res was not set to a valid array.' );
-        }
+		if ( !$res || !is_object( $res ) ) {
+		    throw new MWException( '$res was not set to a valid array.' );
+		}
 
-        foreach ( $res as $row ) {
-            if ( !$row->perm_dbanme || !$row->perm_group ) {
-                continue;
-            }
+		foreach ( $res as $row ) {
+			if ( !$row->perm_dbanme || !$row->perm_group ) {
+				continue;
+			}
 			$dbw->update( 'mw_permissions',
 				[
 					'perm_group' => strtolower( $row->perm_group )
 				],
 				[
-                    'perm_dbname' => $row->perm_dbanme,
+					'perm_dbname' => $row->perm_dbanme,
 					'perm_group' => $row->perm_group
 				],
 				__METHOD__
 			);
-        }
+
+			$dbw_wiki = wfGetDB( DB_MASTER, [], $row->perm_dbanme );
+			$dbw_wiki->update( 'user_groups',
+				[
+					'ug_group' => strtolower( $row->perm_group )
+				],
+				[
+					'ug_group' => $row->perm_group
+				],
+				__METHOD__
+			);
+
+			$dbw_wiki->update( 'user_former_groups',
+				[
+					'ufg_group' => strtolower( $row->perm_group )
+				],
+				[
+					'ufg_group' => $row->perm_group
+				],
+				__METHOD__
+			);
+		}
 	}
 }
 
