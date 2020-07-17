64eaf79f92f33999d337950c8faa808ed23b5c05
paladox <paladox@users.noreply.github.com>
2020-07-17 16:04:12 +0100

    MirahezeIRCRCFeedFormatter: Convert to tabs (#136)

--- includes/MirahezeIRCRCFeedFormatter.php
+++ includes/MirahezeIRCRCFeedFormatter.php
@@ -30,105 +30,105 @@ use MediaWiki\MediaWikiServices;
 
 class MirahezeIRCRCFeedFormatter implements RCFeedFormatter {
 
-    /**
-     * @see RCFeedFormatter::getLine
-     */
-    public function getLine( array $feed, RecentChange $rc, $actionComment ) {
-        $config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'mirahezemagic' );
+	/**
+	 * @see RCFeedFormatter::getLine
+	 */
+	public function getLine( array $feed, RecentChange $rc, $actionComment ) {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'mirahezemagic' );
 
-        $attribs = $rc->getAttributes();
-        if ( $attribs['rc_type'] == RC_LOG ) {
-            // Don't use SpecialPage::getTitleFor, backwards compatibility with
-            // IRC API which expects "Log".
-            $titleObj = Title::newFromText( 'Log/' . $attribs['rc_log_type'], NS_SPECIAL );
-        } else {
-            $titleObj =& $rc->getTitle();
-        }
-        $title = $titleObj->getPrefixedText();
-        $title = self::cleanupForIRC( $title );
+		$attribs = $rc->getAttributes();
+		if ( $attribs['rc_type'] == RC_LOG ) {
+			// Don't use SpecialPage::getTitleFor, backwards compatibility with
+			// IRC API which expects "Log".
+			$titleObj = Title::newFromText( 'Log/' . $attribs['rc_log_type'], NS_SPECIAL );
+		} else {
+			$titleObj =& $rc->getTitle();
+		}
+		$title = $titleObj->getPrefixedText();
+		$title = self::cleanupForIRC( $title );
 
-        if ( $attribs['rc_type'] == RC_LOG ) {
-            $url = '';
-        } else {
-            $url = $config->get( 'CanonicalServer' ) . $config->get( 'Script' );
-            if ( $attribs['rc_type'] == RC_NEW ) {
-                $query = '?oldid=' . $attribs['rc_this_oldid'];
-            } else {
-                $query = '?diff=' . $attribs['rc_this_oldid'] . '&oldid=' . $attribs['rc_last_oldid'];
-            }
-            if ( $config->get( 'UseRCPatrol' ) || ( $attribs['rc_type'] == RC_NEW && $config->get( 'UseNPPatrol' ) ) ) {
-                $query .= '&rcid=' . $attribs['rc_id'];
-            }
-            // HACK: We need this hook for WMF's secure server setup
-            Hooks::run( 'IRCLineURL', array( &$url, &$query, $rc ) );
-            $url .= $query;
-        }
+		if ( $attribs['rc_type'] == RC_LOG ) {
+			$url = '';
+		} else {
+			$url = $config->get( 'CanonicalServer' ) . $config->get( 'Script' );
+			if ( $attribs['rc_type'] == RC_NEW ) {
+				$query = '?oldid=' . $attribs['rc_this_oldid'];
+			} else {
+				$query = '?diff=' . $attribs['rc_this_oldid'] . '&oldid=' . $attribs['rc_last_oldid'];
+			}
+			if ( $config->get( 'UseRCPatrol' ) || ( $attribs['rc_type'] == RC_NEW && $config->get( 'UseNPPatrol' ) ) ) {
+				$query .= '&rcid=' . $attribs['rc_id'];
+			}
+			// HACK: We need this hook for WMF's secure server setup
+			Hooks::run( 'IRCLineURL', array( &$url, &$query, $rc ) );
+			$url .= $query;
+		}
 
-        if ( $attribs['rc_old_len'] !== null && $attribs['rc_new_len'] !== null ) {
-            $szdiff = $attribs['rc_new_len'] - $attribs['rc_old_len'];
-            if ( $szdiff < -500 ) {
-                $szdiff = "\002$szdiff\002";
-            } elseif ( $szdiff >= 0 ) {
-                $szdiff = '+' . $szdiff;
-            }
-            // @todo i18n with parentheses in content language?
-            $szdiff = '(' . $szdiff . ')';
-        } else {
-            $szdiff = '';
-        }
+		if ( $attribs['rc_old_len'] !== null && $attribs['rc_new_len'] !== null ) {
+			$szdiff = $attribs['rc_new_len'] - $attribs['rc_old_len'];
+			if ( $szdiff < -500 ) {
+				$szdiff = "\002$szdiff\002";
+			} elseif ( $szdiff >= 0 ) {
+				$szdiff = '+' . $szdiff;
+			}
+			// @todo i18n with parentheses in content language?
+			$szdiff = '(' . $szdiff . ')';
+		} else {
+			$szdiff = '';
+		}
 
-        $user = self::cleanupForIRC( $attribs['rc_user_text'] );
+		$user = self::cleanupForIRC( $attribs['rc_user_text'] );
 
-        if ( $attribs['rc_type'] == RC_LOG ) {
-            $targetText = $rc->getTitle()->getPrefixedText();
-            $comment = self::cleanupForIRC( str_replace(
-                "[[$targetText]]",
-                "[[\00302$targetText\00310]]",
-                $actionComment
-            ) );
-            $flag = $attribs['rc_log_action'];
-        } else {
-            $comment = self::cleanupForIRC( $attribs['rc_comment'] );
-            $flag = '';
-            if ( !$attribs['rc_patrolled']
-                && ( $config->get( 'UseRCPatrol' ) || $attribs['rc_type'] == RC_NEW && $config->get( 'UseNPPatrol' ) )
-            ) {
-                $flag .= '!';
-            }
-            $flag .= ( $attribs['rc_type'] == RC_NEW ? "N" : "" )
-                . ( $attribs['rc_minor'] ? "M" : "" ) . ( $attribs['rc_bot'] ? "B" : "" );
-        }
+		if ( $attribs['rc_type'] == RC_LOG ) {
+			$targetText = $rc->getTitle()->getPrefixedText();
+			$comment = self::cleanupForIRC( str_replace(
+				"[[$targetText]]",
+				"[[\00302$targetText\00310]]",
+				$actionComment
+			) );
+			$flag = $attribs['rc_log_action'];
+		} else {
+			$comment = self::cleanupForIRC( $attribs['rc_comment'] );
+			$flag = '';
+			if ( !$attribs['rc_patrolled']
+				&& ( $config->get( 'UseRCPatrol' ) || $attribs['rc_type'] == RC_NEW && $config->get( 'UseNPPatrol' ) )
+			) {
+				$flag .= '!';
+			}
+			$flag .= ( $attribs['rc_type'] == RC_NEW ? "N" : "" )
+				. ( $attribs['rc_minor'] ? "M" : "" ) . ( $attribs['rc_bot'] ? "B" : "" );
+		}
 
-        if ( $feed['add_interwiki_prefix'] === true && $config->get( 'LocalInterwikis' ) ) {
-            // we use the first entry in $wgLocalInterwikis in recent changes feeds
-            $prefix = $config->get( 'LocalInterwikis' )[0];
-        } elseif ( $feed['add_interwiki_prefix'] ) {
-            $prefix = $feed['add_interwiki_prefix'];
-        } else {
-            $prefix = false;
-        }
-        if ( $prefix !== false ) {
-            $titleString = "\00314[[\00303$prefix:\00307$title\00314]]";
-        } else {
-            $titleString = "\00314[[\00307$title\00314]]";
-        }
+		if ( $feed['add_interwiki_prefix'] === true && $config->get( 'LocalInterwikis' ) ) {
+			// we use the first entry in $wgLocalInterwikis in recent changes feeds
+			$prefix = $config->get( 'LocalInterwikis' )[0];
+		} elseif ( $feed['add_interwiki_prefix'] ) {
+			$prefix = $feed['add_interwiki_prefix'];
+		} else {
+			$prefix = false;
+		}
+		if ( $prefix !== false ) {
+			$titleString = "\00314[[\00303$prefix:\00307$title\00314]]";
+		} else {
+			$titleString = "\00314[[\00307$title\00314]]";
+		}
 
-        # see http://www.irssi.org/documentation/formats for some colour codes. prefix is \003,
-        # no colour (\003) switches back to the term default
-        return "{$config->get( 'DBname' )} \0035*\003 $titleString\0034 $flag\00310 " .
-            "\00302$url\003 \0035*\003 \00303$user\003 \0035*\003 $szdiff \00310$comment\003\n";
-    }
+		# see http://www.irssi.org/documentation/formats for some colour codes. prefix is \003,
+		# no colour (\003) switches back to the term default
+		return "{$config->get( 'DBname' )} \0035*\003 $titleString\0034 $flag\00310 " .
+			"\00302$url\003 \0035*\003 \00303$user\003 \0035*\003 $szdiff \00310$comment\003\n";
+	}
 
-    /**
-     * Remove newlines, carriage returns and decode html entites
-     * @param string $text
-     * @return string
-     */
-    public static function cleanupForIRC( $text ) {
-        return Sanitizer::decodeCharReferences( str_replace(
-            array( "\n", "\r" ),
-            array( " ", "" ),
-            $text
-        ) );
-    }
+	/**
+	 * Remove newlines, carriage returns and decode html entities
+	 * @param string $text
+	 * @return string
+	 */
+	public static function cleanupForIRC( $text ) {
+		return str_replace(
+			[ "\n", "\r" ],
+			[ " ", "" ],
+			Sanitizer::decodeCharReferences( $text )
+		);
+	}
 }
