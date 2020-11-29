82172df7a4454ae2a8269a3768b017f77c382589
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-11-29 15:24:44 -0700

    fix indentation

--- maintenance/fixUserIdLogging.php
+++ maintenance/fixUserIdLogging.php
@@ -1,92 +1,94 @@
 <?php
-/**
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program; if not, write to the Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-* http://www.gnu.org/copyleft/gpl.html
-*
-* @file
-* @ingroup MirahezeMagic
-* @author Southparkfan
-* @version 1.0
-*/
 
-require_once( "/srv/mediawiki/w/maintenance/Maintenance.php" );
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup MirahezeMagic
+ * @author Southparkfan
+ * @author Universal Omega
+ * @version 1.0
+ */
+
+require_once( __DIR__ . '/../../../maintenance/Maintenance.php' );
 
 use Wikimedia\AtEase\AtEase;
 
 class FixUserIdLogging extends Maintenance {
-        public $mUserCache;
+	public $mUserCache;
 
-        public function __construct() {
-                parent::__construct();
-                $this->addOption( 'fix', 'Actually fix the attribution instead of just checking for wrong entries', false, false );
-                $this->mDescription = 'Fixes user attribution in logs';
-                $this->setBatchSize( 100 );
-        }
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'fix', 'Actually fix the attribution instead of just checking for wrong entries', false, false );
+		$this->mDescription = 'Fixes user attribution in logs';
+		$this->setBatchSize( 100 );
+	}
 
-        public function execute() {
-                $dbr = wfGetDB( DB_REPLICA );
+	public function execute() {
+		$dbr = wfGetDB( DB_REPLICA );
 
-                $start = (int)$dbr->selectField( 'logging', 'MIN(log_id)', false, __METHOD__ );
-                $end = (int)$dbr->selectField( 'logging', 'MAX(log_id)', false, __METHOD__ );
+		$start = (int)$dbr->selectField( 'logging', 'MIN(log_id)', false, __METHOD__ );
+		$end = (int)$dbr->selectField( 'logging', 'MAX(log_id)', false, __METHOD__ );
 
-                $wrongLogs = 0;
+		$wrongLogs = 0;
 
-                $lastCheckedLogId = 0;
+		$lastCheckedLogId = 0;
 
-                do {
-                        $lastCheckedLogIdNextBatch = $lastCheckedLogId + $this->mBatchSize;
-                        $logRes = $dbr->select(
-                                'logging',
-                                [ 'log_id', 'log_params', 'log_actor' ],
-                                "log_id BETWEEN $lastCheckedLogId and $lastCheckedLogIdNextBatch",
-                                __METHOD__
-                        );
+		do {
+			$lastCheckedLogIdNextBatch = $lastCheckedLogId + $this->mBatchSize;
+			$logRes = $dbr->select(
+				'logging',
+				[ 'log_id', 'log_params', 'log_actor' ],
+				"log_id BETWEEN $lastCheckedLogId and $lastCheckedLogIdNextBatch",
+				__METHOD__
+			);
 
-                        foreach ( $logRes as $logRow ) {
+			foreach ( $logRes as $logRow ) {
 				$username = User::newFromActorId( $logRow->log_actor );
-                                $goodUserId = $this->getGoodUserId( $username->getName() );
+				$goodUserId = $this->getGoodUserId( $username->getName() );
 
-                                // Ignore log_actor 0 for maintenance scripts and such
-                                if ( $logRow->log_actor != $goodUserId ) {
-                                        // PANIC EVERYWHERE DON'T DIE ON US
-                                        $wrongLogs++;
+				// Ignore log_actor 0 for maintenance scripts and such
+				if ( $logRow->log_actor != $goodUserId ) {
+					// PANIC EVERYWHERE DON'T DIE ON US
+					$wrongLogs++;
 
-                                        if ( $this->hasOption( 'fix' ) ) {
-                                                $this->fixLogEntry( $logRow );
-                                        }
-                                }
-                        }
+					if ( $this->hasOption( 'fix' ) ) {
+						$this->fixLogEntry( $logRow );
+					}
+				}
+			}
 
 			$lastCheckedLogId = $lastCheckedLogId + $this->mBatchSize;
-                } while ( $lastCheckedLogId <= $end );
+		} while ( $lastCheckedLogId <= $end );
 
-                $line = "$wrongLogs wrong logs detected.";
+		$line = "$wrongLogs wrong logs detected.";
 
-                if ( !$this->hasOption( 'fix' ) ) {
-                        $line .= " Run this script with --fix to actually fix the log entries.";
-                }
+		if ( !$this->hasOption( 'fix' ) ) {
+			$line .= " Run this script with --fix to actually fix the log entries.";
+		}
 
-                $this->output( $line . "\n" );
-        }
+		$this->output( $line . "\n" );
+	}
 
-        public function getGoodUserId( $username ) {
-                $whitelist = [ 'Maintenance script', 'MediaWiki default' ];
+	public function getGoodUserId( $username ) {
+		$whitelist = [ 'Maintenance script', 'MediaWiki default' ];
 
-                if ( in_array( $username, $whitelist ) ) {
-                        return 0;
-                }
+		if ( in_array( $username, $whitelist ) ) {
+			return 0;
+		}
 
 
 		if ( isset( $this->mUserCache[$username] ) ) {
@@ -94,57 +96,57 @@ class FixUserIdLogging extends Maintenance {
 		} else {
 			$dbr = wfGetDB( DB_REPLICA );
 	
-	                $userId = $dbr->selectField(
-        	                        'user',
-                	               	'user_id',
-                        	        [ 'user_name' => $username ],
-                                	__METHOD__
-	                );
+			$userId = $dbr->selectField(
+					'user',
+				       	'user_id',
+					[ 'user_name' => $username ],
+					__METHOD__
+			);
 
 
-	                if ( ( is_string( $userId ) || is_numeric( $userId ) ) && $userId !== 0 ) {
+			if ( ( is_string( $userId ) || is_numeric( $userId ) ) && $userId !== 0 ) {
 				$goodUserId = $userId;
-                	} else {
-                                $goodUserId = 0;
-	                }
+			} else {
+				$goodUserId = 0;
+			}
 
 			$this->mUserCache[$username] = $goodUserId;
 		}
 		
-        	        return $goodUserId;
+			return $goodUserId;
 
 	}
 
-        protected function fixLogEntry( $row ) {
+	protected function fixLogEntry( $row ) {
 		$username = User::newFromActorId( $logRow->log_actor );
 
-                $dbr = wfGetDB( DB_REPLICA );
-                $dbw = wfGetDB( DB_MASTER );
+		$dbr = wfGetDB( DB_REPLICA );
+		$dbw = wfGetDB( DB_MASTER );
 
-                if ( isset( $this->mUserCache[$username->getName()] ) ) {
-                        $userId = $this->mUserCache[$username->getName()];
-                } else {
-                        $userId = $dbr->selectField(
-                                'user',
-                                'user_id',
-                                [ 'user_name' => $username->getName() ],
-                                __METHOD__
-                        );
+		if ( isset( $this->mUserCache[$username->getName()] ) ) {
+			$userId = $this->mUserCache[$username->getName()];
+		} else {
+			$userId = $dbr->selectField(
+				'user',
+				'user_id',
+				[ 'user_name' => $username->getName() ],
+				__METHOD__
+			);
 
-                        $this->mUserCache[$username->getName()] = $userId;
-                }
+			$this->mUserCache[$username->getName()] = $userId;
+		}
 
-                if ( !( ( is_string( $userId ) || is_numeric( $userId ) ) && $userId !== 0 ) ) {
-                        $userId = 0;
-                }
+		if ( !( ( is_string( $userId ) || is_numeric( $userId ) ) && $userId !== 0 ) ) {
+			$userId = 0;
+		}
 
-                AtEase::suppressWarnings();
-                $logParams = unserialize( $row->log_params );
-                AtEase::restoreWarnings();
+		AtEase::suppressWarnings();
+		$logParams = unserialize( $row->log_params );
+		AtEase::restoreWarnings();
 
-                if ( is_array( $logParams ) && isset( $logParams['4::userid'] ) ) {
-                        $logParams['4::userid'] = $userId;
-                }
+		if ( is_array( $logParams ) && isset( $logParams['4::userid'] ) ) {
+			$logParams['4::userid'] = $userId;
+		}
 
 
 		$updateParams = [
@@ -162,8 +164,8 @@ class FixUserIdLogging extends Maintenance {
 			__METHOD__
 		);
 
-                $this->output( "Done! Updated log_id {$row->log_id} to have the log_actor id {$userId} (for '{$username->getName()}') instead of {$row->log_actor}.\n" );
-        }
+		$this->output( "Done! Updated log_id {$row->log_id} to have the log_actor id {$userId} (for '{$username->getName()}') instead of {$row->log_actor}.\n" );
+	}
 }
 
 $maintClass = 'FixUserIdLogging';
