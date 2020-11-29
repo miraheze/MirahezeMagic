dff5deceb8016be41fabdfe8151a162c13141c54
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-11-29 15:38:57 -0700

    add comment

--- maintenance/getJobQueueLengths.php
+++ maintenance/getJobQueueLengths.php
@@ -1,11 +1,31 @@
 <?php
 
 /**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup MirahezeMagic
+ * @author John Lewis
+ * @version 1.0
+ *
  * Get the length of the job queue on all wikis in $wgConf
  * Sourced from WikimediaMaintenance
  */
 
-require_once( '/srv/mediawiki/w/maintenance/Maintenance.php' );
+require_once( __DIR__ . '/../../../maintenance/Maintenance.php' );
 
 use MediaWiki\MediaWikiServices;
 
