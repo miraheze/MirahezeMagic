955bf6ef809776cbaf2648216ef6b058bfbc81a6
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-05 14:36:45 -0700

    fix casing

--- maintenance/removePII.php
+++ maintenance/removePII.php
@@ -647,7 +647,7 @@ class RemovePII extends Maintenance {
 		$wgUser = $user;
 
 		// Hide deletions from RecentChanges
-		$UserGroupManager->addUserToGroup( $user, 'bot' );
+		$userGroupManager->addUserToGroup( $user, 'bot' );
 
 		$error = '';
 		$title = Title::newFromText( $oldName->getTitleKey(), NS_USER );
