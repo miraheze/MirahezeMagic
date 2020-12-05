145f5f1d461a545ff5f330ea24d4735d3cff3d2b
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-05 14:23:38 -0700

    convert addGroup to UserGroupManager

--- maintenance/removePII.php
+++ maintenance/removePII.php
@@ -636,23 +636,23 @@ class RemovePII extends Maintenance {
 		);
 
 		$user = User::newSystemUser( 'MediaWiki default', [ 'steal' => true ] );
+
+		$UserGroupManager = MediaWikiServices::getInstance()->getUserGroupManager();
+
 		if ( !$user ) {
 			$this->fatalError( "Invalid username" );
 		}
+
 		global $wgUser;
 		$wgUser = $user;
 
 		// Hide deletions from RecentChanges
-		$user->addGroup( 'bot' );
+		$UserGroupManager->addUserToGroup( $user, 'bot' );
 
 		$error = '';
 		$title = Title::newFromText( $oldName->getTitleKey(), NS_USER );
 		$userPage = WikiPage::factory( $title );
-		if ( version_compare( MW_VERSION, '1.35', '<' ) ) {
-			$status = $userPage->doDeleteArticleReal( '', true, null, null, $error, $user );
-		} else {
-			$status = $userPage->doDeleteArticleReal( '', $user );
-		}
+		$status = $userPage->doDeleteArticleReal( '', $user );
 
 		if ( !$status->isOK() ) {
 			$errorMessage = json_encode( $status->getErrorsByType( 'error' ) );
