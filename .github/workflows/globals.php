50b862f84615ee3dab1e23db6bbad0c4a648407e
RhinosF1 <rhinosf1@miraheze.org>
2022-02-22 14:09:36 +0000

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -35,6 +35,7 @@ $wgHooks['MediaWikiServices'][] = 'wfOnMediaWikiServices';
 
 function wfOnMediaWikiServices( MediaWiki\MediaWikiServices $services ) {
 		$dbw = wfGetDB( DB_PRIMARY );
+		$dbw->startAtomic( __METHOD__ );
 		$dbw->insert(
 			'cw_wikis',
 			[
@@ -55,6 +56,7 @@ function wfOnMediaWikiServices( MediaWiki\MediaWikiServices $services ) {
                         __METHOD__,
                         [ 'IGNORE' ],
 		);
+		$dbw->endAtomic( __METHOD__ );
 }
 
 $wi->readCache();
