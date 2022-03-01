c10849876a642ca3b08ec01460900285dc16f23e
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-03-01 11:58:45 -0700

    Update assignImportedEdits.php

--- maintenance/assignImportedEdits.php
+++ maintenance/assignImportedEdits.php
@@ -88,7 +88,7 @@ class AssignImportedEdits extends Maintenance {
 	private function assignEdits( &$user, &$importUser ) {
 		$this->output(
 			"Assigning imported edits from " . ( strpos( $user, $this->importPrefix ) === false ? $this->importPrefix : null ) . "{$user->getName()} to {$importUser->getName()}\n"
- );
+		);
 
 		$dbw = $this->getDB( DB_PRIMARY );
 		$this->beginTransaction( $dbw, __METHOD__ );
