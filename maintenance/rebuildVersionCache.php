652886cdcbd4cf953a74c4eb5d53151ab5e39b6c
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-29 17:26:02 -0600

    support 1.38 in rebuildVersionCache (#337)
    * support 1.38 in rebuildVersionCache
    
    * Update rebuildVersionCache.php

--- maintenance/rebuildVersionCache.php
+++ maintenance/rebuildVersionCache.php
@@ -45,20 +45,25 @@ class RebuildVersionCache extends Maintenance {
 	}
 
 	public function execute() {
-		global $IP, $wgShellRestrictionMethod;
+		global $IP;
 
-		$IP = '/srv/mediawiki-staging/w';
-		$wgShellRestrictionMethod = false;
+		$config = new HashConfig();
+		$config->set( 'BaseDirectory', '/srv/mediawiki-staging/w' );
+		$config->set( 'ShellRestrictionMethod', false );
 
-		$gitInfo = new GitInfo( $IP, false );
+		$baseDirectory = $config->get( 'BaseDirectory' );
+
+		$IP = $baseDirectory;
+
+		$gitInfo = new GitInfo( $baseDirectory, false );
 		$gitInfo->precomputeValues();
 
 		$cache = ObjectCache::getInstance( CACHE_ANYTHING );
 		$coreId = $gitInfo->getHeadSHA1() ?: '';
 
 		$queue = array_fill_keys( array_merge(
-				glob( $IP . '/extensions/*/extension*.json' ),
-				glob( $IP . '/skins/*/skin.json' )
+				glob( $baseDirectory . '/extensions/*/extension*.json' ),
+				glob( $baseDirectory . '/skins/*/skin.json' )
 			),
 		true );
 
@@ -82,7 +87,7 @@ class RebuildVersionCache extends Maintenance {
 		foreach ( $extensionCredits as $extension => $extensionData ) {
 			if ( isset( $extensionData['path'] ) ) {
 				$extensionDirectory = dirname( $extensionData['path'] );
-				$extensionPath = str_replace( '/srv/mediawiki/w', $IP, $extensionDirectory );
+				$extensionPath = str_replace( '/srv/mediawiki/w', $baseDirectory, $extensionDirectory );
 
 				$gitInfo = new GitInfo( $extensionPath, false );
 
@@ -91,7 +96,7 @@ class RebuildVersionCache extends Maintenance {
 				}
 
 				$memcKey = $cache->makeKey(
-					'specialversion-ext-version-text', str_replace( $IP, '/srv/mediawiki/w', $extensionData['path'] ), $coreId
+					'specialversion-ext-version-text', str_replace( $baseDirectory, '/srv/mediawiki/w', $extensionData['path'] ), $coreId
 				);
 
 				$cache->delete( $memcKey );
