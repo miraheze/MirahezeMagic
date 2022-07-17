13ad30653323e8a1ef9d8c702a0ccab6347323ca
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-07-17 10:14:27 -0600

    CI: fix support for MediaWiki 1.39+ (#352)

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -7,7 +7,16 @@ function wfOnMediaWikiServices( MediaWiki\MediaWikiServices $services ) {
 		global $IP;
 		$dbw = wfGetDB( DB_PRIMARY );
 		if ( !$dbw->tableExists( 'echo_unread_wikis' ) ) {
-			$dbw->sourceFile( "$IP/extensions/Echo/db_patches/echo_unread_wikis.sql" );
+			// < MediaWiki 1.39 — Remove once CI drops MediaWiki 1.38 support
+			if ( file_exists( "$IP/extensions/Echo/db_patches/echo_unread_wikis.sql" ) ) {
+				$dbw->sourceFile( "$IP/extensions/Echo/db_patches/echo_unread_wikis.sql" );
+				return;
+			}
+
+			// MediaWiki 1.39+
+			if ( file_exists( "$IP/extensions/Echo/sql/mysql/tables-sharedtracking-generated.sql" ) ) {
+				$dbw->sourceFile( "$IP/extensions/Echo/sql/mysql/tables-sharedtracking-generated.sql" );
+			}
 		}
 	} catch ( Wikimedia\Rdbms\DBQueryError $e ) {
 		return;
