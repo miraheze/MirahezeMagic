b52215a45894115ddc6a6099997bf6c60453a3c3
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-01-12 11:16:57 -0700

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -16,20 +16,20 @@ function wfOnMediaWikiServices( MediaWiki\MediaWikiServices $services ) {
 			$dbw ??= $services->getDBLoadBalancer()
 				->getMaintenanceConnectionRef( DB_PRIMARY );
 
-			$dbw->startAtomic();
-
 			// < MediaWiki 1.39 — Remove once CI drops MediaWiki 1.38 support
 			if ( file_exists( "$IP/extensions/Echo/db_patches/echo_unread_wikis.sql" ) ) {
+				$dbw->startAtomic();
 				$dbw->sourceFile( "$IP/extensions/Echo/db_patches/echo_unread_wikis.sql" );
+				$dbw->endAtomic();
 				return;
 			}
 
 			// MediaWiki 1.39+
 			if ( file_exists( "$IP/extensions/Echo/sql/mysql/tables-sharedtracking-generated.sql" ) ) {
+				$dbw->startAtomic();
 				$dbw->sourceFile( "$IP/extensions/Echo/sql/mysql/tables-sharedtracking-generated.sql" );
+				$dbw->endAtomic();
 			}
-
-			$dbw->endAtomic();
 		}
 	} catch ( Wikimedia\Rdbms\DBQueryError $e ) {
 		return;
