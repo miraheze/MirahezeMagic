3c6bec64f81896d6f60a0b556e3209ac712038a5
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-01-12 14:23:37 -0700

    CI: use the `wfInitDBConnection()` function

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -2,22 +2,18 @@
 
 use MediaWiki\MediaWikiServices;
 use Wikimedia\Rdbms\DBQueryError;
-use Wikimedia\Rdbms\DBUnexpectedError;
 
 $wgHooks['MediaWikiServices'][] = 'wfOnMediaWikiServices';
 
 function wfOnMediaWikiServices( MediaWikiServices $services ) {
 	try {
 		global $IP;
-
-		static $dbw = null;
-		$dbw ??= $services->getDBLoadBalancer()
-			->getMaintenanceConnectionRef( DB_PRIMARY );
+		$dbw = wfInitDBConnection();
 
 		if ( !$dbw->tableExists( 'echo_unread_wikis' ) ) {
 			$dbw->sourceFile( "$IP/extensions/Echo/sql/mysql/tables-sharedtracking-generated.sql" );
 		}
-	} catch ( DBQueryError | DBUnexpectedError $e ) {
+	} catch ( DBQueryError $e ) {
 		return;
 	}
 }
