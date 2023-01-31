e0e973917285049bb063a21bb9cb08a632160e15
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-01-31 10:41:44 -0700

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -3,6 +3,22 @@
 use MediaWiki\MediaWikiServices;
 use Wikimedia\Rdbms\DBQueryError;
 
+$wgObjectCaches['memcached'] = [
+	'class'                => MemcachedPeclBagOStuff::class,
+	'serializer'           => 'php',
+	'persistent'           => false,
+	'servers'              => [ '127.0.0.1:11211' ],
+	// Effectively disable the failure limit (0 is invalid)
+	'server_failure_limit' => 1e9,
+	// Effectively disable the retry timeout
+	'retry_timeout'        => -1,
+	'loggroup'             => 'memcached',
+	// 500ms, in microseconds
+	'timeout'              => 1 * 1e6,
+];
+
+$wgMainCacheType = 'memcached';
+
 $wgHooks['MediaWikiServices'][] = 'wfOnMediaWikiServices';
 
 function wfOnMediaWikiServices( MediaWikiServices $services ) {
