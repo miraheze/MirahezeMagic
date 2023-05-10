d926c79433b500955b844cea3ec8f1c35ba1d7e7
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-05-09 23:19:51 -0500

    Fix i18n for suppress group (#422)

--- includes/MirahezeMagicHooks.php
+++ includes/MirahezeMagicHooks.php
@@ -347,8 +347,6 @@ class MirahezeMagicHooks {
 			'grouppage-bot',
 			'group-bot-member',
 			'grouppage-user',
-			'group-suppress',
-			'group-suppress-member',
 			'grouppage-suppress',
 			'webauthn-module-description',
 		];
