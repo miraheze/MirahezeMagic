86583cec69b42c1da180010e27304e997fc01934
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-05-10 10:52:42 -0500

    Add licensing info and credit for VanishUser

--- includes/SpecialVanishUser.php
+++ includes/SpecialVanishUser.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * Creates Special:VanishUser for Stewards to use to vanish users easily.
+ * Derived from RemovePII's Special:RemovePII code located at
+ * https://github.com/miraheze/RemovePII/blob/master/includes/SpecialRemovePII.php
+ * Originally written by Universal Omega.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @author Universal Omega
+ * @author Agent Isai
+ * @version 1.0
+ */
 
 use ExtensionRegistry;
 use FormSpecialPage;
