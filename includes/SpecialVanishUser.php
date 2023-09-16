4fb1a1c32ecbd2115c75d652971d082f20675cf0
paladox <paladox@users.noreply.github.com>
2023-09-16 14:17:00 +0100

    Update SpecialVanishUser.php

--- includes/SpecialVanishUser.php
+++ includes/SpecialVanishUser.php
@@ -115,7 +115,9 @@ class SpecialVanishUser extends FormSpecialPage {
 			return Status::newFatal( 'removepii-centralauth-notinstalled' );
 		}
 
-		if ( version_compare( MW_VERSION, '1.40', '<' ) && !ExtensionRegistry::getInstance()->isLoaded( 'Renameuser' ) ) {
+		if ( version_compare( MW_VERSION, '1.40', '<' ) &&
+		    !ExtensionRegistry::getInstance()->isLoaded( 'Renameuser' )
+		) {
 			return Status::newFatal( 'centralauth-rename-notinstalled' );
 		}
 
