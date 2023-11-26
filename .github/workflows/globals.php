5d4b31fbbcfb08f54e9f4205f260d49e97ab8e7a
paladox <paladox@users.noreply.github.com>
2023-11-26 13:07:48 -0500

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -7,19 +7,12 @@ $wgHooks['MediaWikiServices'][] = 'wfOnMediaWikiServices';
 
 function wfOnMediaWikiServices( MediaWikiServices $services ) {
 	try {
-		if ( getenv( 'WIKI_ECHO_CREATION_SQL_EXECUTED' ) ) {
-			return;
-		}
-
 		global $IP;
 		$dbw = wfInitDBConnection();
-		$dbw->selectDomain( 'wikidb' );
 
 		if ( !$dbw->tableExists( 'echo_unread_wikis' ) ) {
 			$dbw->sourceFile( "$IP/extensions/Echo/sql/mysql/tables-sharedtracking-generated.sql" );
 		}
-
-		putenv( 'WIKI_ECHO_CREATION_SQL_EXECUTED=true' );
 	} catch ( DBQueryError $e ) {
 		return;
 	}
