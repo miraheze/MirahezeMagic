4a4863265200c98af557e5103a3e1c76a329c535
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-24 14:09:08 +0800

    Fix

--- includes/MirahezeIRCRCFeedFormatter.php
+++ includes/MirahezeIRCRCFeedFormatter.php
@@ -118,7 +118,7 @@ class MirahezeIRCRCFeedFormatter implements RCFeedFormatter {
 			$comment = self::cleanupForIRC( $store->getComment( 'rc_comment', $attribs )->text );
 			$flag = '';
 			if ( !$attribs['rc_patrolled']
-				&& ( $useRCPatrol || $attribs['rc_type'] == RC_NEW && $useNPPatrol )
+				&& ( $useRCPatrol || ( $attribs['rc_type'] == RC_NEW && $useNPPatrol ) )
 			) {
 				$flag .= '!';
 			}
