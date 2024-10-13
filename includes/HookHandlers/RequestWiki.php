e2bab2236d1efa8d47192b426cc38eb763a0e77f
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-14 02:20:27 +0800

    Cleanup

--- includes/HookHandlers/RequestWiki.php
+++ includes/HookHandlers/RequestWiki.php
@@ -15,54 +15,47 @@ class RequestWiki implements
 {
 
 	public function onRequestWikiFormDescriptorModify( array &$formDescriptor ): void {
-		$nsfwField = [
-			'label-message' => 'requestwiki-label-nsfw',
-			'help-message' => 'requestwiki-help-nsfw',
-			'type' => 'check',
-		];
-
 		RequestWikiFormUtils::insertFieldAfter(
 			$formDescriptor,
 			afterKey: 'bio',
 			newKey: 'nsfw',
-			newField: $nsfwField
+			newField: [
+				'label-message' => 'requestwiki-label-nsfw',
+				'help-message' => 'requestwiki-help-nsfw',
+				'type' => 'check',
+			]
 		);
-
-		$nsfwtextField = [
-			'label-message' => 'requestwiki-label-nsfwtext',
-			'hide-if' => [ '!==', 'nsfw', '1' ],
-			'type' => 'text',
-		];
 
 		RequestWikiFormUtils::insertFieldAfter(
 			$formDescriptor,
 			afterKey: 'nsfw',
 			newKey: 'nsfwtext',
-			newField: $nsfwtextField
+			newField: [
+				'label-message' => 'requestwiki-label-nsfwtext',
+				'hide-if' => [ '!==', 'nsfw', '1' ],
+				'type' => 'text',
+			]
 		);
-		$sourceField = [
-			'label-message' => 'requestwiki-label-source',
-			'type' => 'check',
-		];
 
 		RequestWikiFormUtils::insertFieldAfter(
 			$formDescriptor,
 			afterKey: 'purpose',
 			newKey: 'source',
-			newField: $sourceField
+			newField: [
+				'label-message' => 'requestwiki-label-source',
+				'type' => 'check',
+			]
 		);
-
-		$sourceurlField = [
-			'label-message' => 'requestwiki-label-sourceurl',
-			'hide-if' => [ '!==', 'source', '1' ],
-			'type' => 'url',
-		];
 
 		RequestWikiFormUtils::insertFieldAfter(
 			$formDescriptor,
 			afterKey: 'source',
 			newKey: 'sourceurl',
-			newField: $sourceurlField
+			newField: [
+				'label-message' => 'requestwiki-label-sourceurl',
+				'hide-if' => [ '!==', 'source', '1' ],
+				'type' => 'url',
+			]
 		);
 	}
 
@@ -71,90 +64,86 @@ class RequestWiki implements
 		User $user,
 		WikiRequestManager $wikiRequestManager
 	): void {
-		$nsfwField = [
-			'label-message' => 'requestwiki-label-nsfw',
-			'type' => 'check',
-			'section' => 'editing',
-			'default' => $wikiRequestManager->getExtraFieldData( 'nsfw' ),
-		];
-
 		RequestWikiFormUtils::insertFieldAfter(
 			$formDescriptor,
 			afterKey: 'edit-bio',
 			newKey: 'edit-nsfw',
-			newField: $nsfwField
+			newField: [
+				'label-message' => 'requestwiki-label-nsfw',
+				'type' => 'check',
+				'section' => 'editing',
+				'default' => $wikiRequestManager->getExtraFieldData( 'nsfw' ),
+			]
 		);
-
-		$nsfwtextField = [
-			'label-message' => 'requestwiki-label-nsfwtext',
-			'type' => 'text',
-			'section' => 'editing',
-			'hide-if' => [ '!==', 'edit-nsfw', '1' ],
-			'default' => $wikiRequestManager->getExtraFieldData( 'nsfwtext' ),
-		];
 
 		RequestWikiFormUtils::insertFieldAfter(
 			$formDescriptor,
 			afterKey: 'edit-nsfw',
 			newKey: 'edit-nsfwtext',
-			newField: $nsfwtextField
+			newField: [
+				'label-message' => 'requestwiki-label-nsfwtext',
+				'type' => 'text',
+				'section' => 'editing',
+				'hide-if' => [ '!==', 'edit-nsfw', '1' ],
+				'default' => $wikiRequestManager->getExtraFieldData( 'nsfwtext' ),
+			]
 		);
 
-		$nsfwField = [
-			'label-message' => 'requestwiki-label-nsfw',
-			'type' => 'info',
-			'section' => 'details',
-			'raw' => true,
-			'default' => ( new RawMessage( ( $wikiRequestManager->getExtraFieldData( 'nsfw' ) ? '{{Done|Yes}}' : '{{Notdone|No}}' ) ) )->parse(), ];
+		$isNsfw = $wikiRequestManager->getExtraFieldData( 'nsfw' );
+		$nsfwMessage = new RawMessage( $isNsfw ? '{{Done|Yes}}' : '{{Notdone|No}}' );
 
 		RequestWikiFormUtils::insertFieldAfter(
 			$formDescriptor,
 			afterKey: 'details-description',
 			newKey: 'details-nsfw',
-			newField: $nsfwField
+			newField: [
+				'label-message' => 'requestwiki-label-nsfw',
+				'type' => 'info',
+				'section' => 'details',
+				'raw' => true,
+				'default' => $nsfwMessage->parse(),
+			]
 		);
-
-		$sourceField = [
-			'label-message' => 'requestwiki-label-source',
-			'type' => 'check',
-			'section' => 'editing',
-			'default' => $wikiRequestManager->getExtraFieldData( 'source' ),
-		];
 
 		RequestWikiFormUtils::insertFieldAfter(
 			$formDescriptor,
 			afterKey: 'edit-purpose',
 			newKey: 'edit-source',
-			newField: $sourceField
+			newField: [
+				'label-message' => 'requestwiki-label-source',
+				'type' => 'check',
+				'section' => 'editing',
+				'default' => $wikiRequestManager->getExtraFieldData( 'source' ),
+			]
 		);
-
-		$sourceurlField = [
-			'label-message' => 'requestwiki-label-sourceurl',
-			'type' => 'url',
-			'section' => 'editing',
-			'hide-if' => [ '!==', 'edit-source', '1' ],
-			'default' => $wikiRequestManager->getExtraFieldData( 'sourceurl' ),
-		];
 
 		RequestWikiFormUtils::insertFieldAfter(
 			$formDescriptor,
 			afterKey: 'edit-source',
 			newKey: 'edit-sourceurl',
-			newField: $sourceurlField
+			newField: [
+				'label-message' => 'requestwiki-label-sourceurl',
+				'type' => 'url',
+				'section' => 'editing',
+				'hide-if' => [ '!==', 'edit-source', '1' ],
+				'default' => $wikiRequestManager->getExtraFieldData( 'sourceurl' ),
+			]
 		);
 
-		$sourceField = [
-			'label-message' => 'requestwiki-label-source',
-			'type' => 'info',
-			'section' => 'details',
-			'raw' => true,
-			'default' => ( new RawMessage( ( $wikiRequestManager->getExtraFieldData( 'source' ) ? '{{Done|Yes}}' : '{{Notdone|No}}' ) ) )->parse(), ];
+		$hasSource = $wikiRequestManager->getExtraFieldData( 'source' );
+		$sourceMessage = new RawMessage( $hasSource ? '{{Done|Yes}}' : '{{Notdone|No}}' );
 
 		RequestWikiFormUtils::insertFieldAfter(
 			$formDescriptor,
 			afterKey: 'details-purpose',
 			newKey: 'details-source',
-			newField: $sourceField
+			newField: [
+				'label-message' => 'requestwiki-label-source',
+				'type' => 'info',
+				'section' => 'details',
+				'raw' => true,
+				'default' => $sourceMessage->parse(),
+			]
 		);
 	}
 }
