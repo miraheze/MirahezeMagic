3d671082d540479877f000c2c230a01d4ac45469
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-14 11:38:30 -0600

    Update queue

--- includes/HookHandlers/RequestWiki.php
+++ includes/HookHandlers/RequestWiki.php
@@ -299,28 +299,234 @@ class RequestWiki implements
 		User $user,
 		WikiRequestManager $wikiRequestManager
 	): void {
-		RequestWikiFormUtils::insertFieldAfter(
+		RequestWikiFormUtils::addFieldToEnd(
 			$formDescriptor,
-			afterKey: 'edit-bio',
 			newKey: 'edit-nsfw',
 			newField: [
 				'label-message' => 'requestwiki-label-nsfw',
-				'type' => 'check',
+				'help-message' => 'requestwiki-help-nsfw',
+				'help-inline' => false,
 				'section' => 'editing/info',
+				'type' => 'check',
 				'default' => $wikiRequestManager->getExtraFieldData( 'nsfw' ),
 			]
 		);
 
-		RequestWikiFormUtils::insertFieldAfter(
+		RequestWikiFormUtils::addFieldToEnd(
 			$formDescriptor,
-			afterKey: 'edit-nsfw',
 			newKey: 'edit-nsfwtext',
 			newField: [
 				'label-message' => 'requestwiki-label-nsfwtext',
-				'type' => 'text',
-				'section' => 'editing',
 				'hide-if' => [ '!==', 'edit-nsfw', '1' ],
+				'section' => 'editing/info',
+				'type' => 'text',
 				'default' => $wikiRequestManager->getExtraFieldData( 'nsfwtext' ),
+			]
+		);
+
+		RequestWikiFormUtils::addFieldToEnd(
+			$formDescriptor,
+			newKey: 'edit-source',
+			newField: [
+				'label-message' => 'requestwiki-label-source',
+				'section' => 'editing/info',
+				'type' => 'check',
+				'default' => $wikiRequestManager->getExtraFieldData( 'source' ),
+			]
+		);
+
+		RequestWikiFormUtils::addFieldToEnd(
+			$formDescriptor,
+			newKey: 'edit-sourceurl',
+			newField: [
+				'label-message' => 'requestwiki-label-sourceurl',
+				'hide-if' => [ '!==', 'edit-source', '1' ],
+				'section' => 'editing/info',
+				'type' => 'url',
+				'default' => $wikiRequestManager->getExtraFieldData( 'sourceurl' ),
+			]
+		);
+
+		RequestWikiFormUtils::addFieldToEnd(
+			$formDescriptor,
+			newKey: 'edit-defaultskin',
+			newField: [
+				'type' => 'select',
+				'label-message' => 'requestwiki-label-defaultskin',
+				'options' => array_combine(
+					$this->options->get( 'MirahezeMagicRequestWikiSkins' ),
+					$this->options->get( 'MirahezeMagicRequestWikiSkins' )
+				),
+				'section' => 'editing/configure',
+				'default' => $wikiRequestManager->getExtraFieldData( 'defaultskin' ),
+			]
+		);
+
+		RequestWikiFormUtils::addFieldToEnd(
+			$formDescriptor,
+			newKey: 'edit-showadvanced',
+			newField: [
+				'type' => 'check',
+				'label-message' => 'requestwiki-label-showadvanced',
+				'section' => 'editing/advanced',
+				'default' => $wikiRequestManager->getExtraFieldData( 'showadvanced' ),
+			]
+		);
+
+		RequestWikiFormUtils::addFieldToEnd(
+			$formDescriptor,
+			newKey: 'edit-articlepath',
+			newField: [
+				'type' => 'radio',
+				'label-message' => 'miraheze-label-managewiki-article-path',
+				'options-messages' => [
+					'miraheze-label-managewiki-article-path-wiki' => '/wiki/$1',
+					'miraheze-label-managewiki-article-path-root' => '/$1',
+				],
+				'hide-if' => [ '!==', 'edit-showadvanced', '1' ],
+				'section' => 'editing/advanced',
+				'default' => $wikiRequestManager->getExtraFieldData( 'articlepath' ),
+			]
+		);
+
+		RequestWikiFormUtils::addFieldToEnd(
+			$formDescriptor,
+			newKey: 'edit-defaultextensions',
+			newField: [
+				'type' => 'multiselect',
+				'label-message' => 'requestwiki-label-defaultextensions',
+				'help-message' => 'requestwiki-help-defaultextensions',
+				'help-inline' => false,
+				'options' => array_combine(
+					$this->options->get( 'MirahezeMagicRequestWikiExtensions' ),
+					$this->options->get( 'MirahezeMagicRequestWikiExtensions' )
+				),
+				'hide-if' => [ '!==', 'edit-showadvanced', '1' ],
+				'dropdown' => true,
+				'section' => 'editing/advanced',
+				'default' => $wikiRequestManager->getExtraFieldData( 'defaultextensions' ),
+			]
+		);
+
+		RequestWikiFormUtils::moveFieldToSection(
+			$formDescriptor,
+			fieldKey: 'edit-subdomain',
+			newSection: 'editing/core'
+		);
+
+		RequestWikiFormUtils::moveFieldToSection(
+			$formDescriptor,
+			fieldKey: 'edit-sitename',
+			newSection: 'editing/core'
+		);
+
+		RequestWikiFormUtils::moveFieldToSection(
+			$formDescriptor,
+			fieldKey: 'edit-language',
+			newSection: 'editing/core'
+		);
+
+		RequestWikiFormUtils::moveFieldToSection(
+			$formDescriptor,
+			fieldKey: 'edit-category',
+			newSection: 'editing/configure'
+		);
+
+		RequestWikiFormUtils::moveFieldToSection(
+			$formDescriptor,
+			fieldKey: 'edit-private',
+			newSection: 'editing/configure'
+		);
+
+		RequestWikiFormUtils::moveFieldToSection(
+			$formDescriptor,
+			fieldKey: 'edit-purpose',
+			newSection: 'editing/info'
+		);
+
+		RequestWikiFormUtils::moveFieldToSection(
+			$formDescriptor,
+			fieldKey: 'edit-reason',
+			newSection: 'editing/info'
+		);
+
+		RequestWikiFormUtils::moveFieldToSection(
+			$formDescriptor,
+			fieldKey: 'edit-bio',
+			newSection: 'editing/info'
+		);
+
+		RequestWikiFormUtils::updateFieldProperties(
+			$formDescriptor,
+			fieldKey: 'edit-category',
+			newProperties: [ 'help-inline' => false ]
+		);
+
+		RequestWikiFormUtils::updateFieldProperties(
+			$formDescriptor,
+			fieldKey: 'edit-subdomain',
+			newProperties: [ 'help-inline' => false ]
+		);
+
+		RequestWikiFormUtils::updateFieldProperties(
+			$formDescriptor,
+			fieldKey: 'edit-sitename',
+			newProperties: [ 'help-inline' => false ]
+		);
+
+		RequestWikiFormUtils::updateFieldProperties(
+			$formDescriptor,
+			fieldKey: 'edit-private',
+			newProperties: [ 'help-inline' => false ]
+		);
+
+		RequestWikiFormUtils::updateFieldProperties(
+			$formDescriptor,
+			fieldKey: 'edit-bio',
+			newProperties: [ 'help-inline' => false ]
+		);
+
+		RequestWikiFormUtils::reorderFieldsInSection(
+			$formDescriptor,
+			section: 'editing/core',
+			newOrder: [
+				'edit-subdomain',
+				'edit-sitename',
+				'edit-language',
+			]
+		);
+
+		RequestWikiFormUtils::reorderFieldsInSection(
+			$formDescriptor,
+			section: 'editing/info',
+			newOrder: [
+				'edit-purpose',
+				'edit-bio',
+				'edit-nsfw',
+				'edit-source',
+				'edit-nsfwtext',
+				'edit-sourceurl',
+				'edit-reason',
+			]
+		);
+
+		RequestWikiFormUtils::reorderFieldsInSection(
+			$formDescriptor,
+			section: 'editing/configure',
+			newOrder: [
+				'edit-private',
+				'edit-category',
+				'edit-defaultskin',
+			]
+		);
+
+		RequestWikiFormUtils::reorderFieldsInSection(
+			$formDescriptor,
+			section: 'editing/advanced',
+			newOrder: [
+				'edit-showadvanced',
+				'edit-articlepath',
+				'edit-defaultextensions',
 			]
 		);
 
@@ -337,31 +543,6 @@ class RequestWiki implements
 				'section' => 'details',
 				'raw' => true,
 				'default' => $nsfwMessage->parse(),
-			]
-		);
-
-		RequestWikiFormUtils::insertFieldAfter(
-			$formDescriptor,
-			afterKey: 'edit-purpose',
-			newKey: 'edit-source',
-			newField: [
-				'label-message' => 'requestwiki-label-source',
-				'type' => 'check',
-				'section' => 'editing',
-				'default' => $wikiRequestManager->getExtraFieldData( 'source' ),
-			]
-		);
-
-		RequestWikiFormUtils::insertFieldAfter(
-			$formDescriptor,
-			afterKey: 'edit-source',
-			newKey: 'edit-sourceurl',
-			newField: [
-				'label-message' => 'requestwiki-label-sourceurl',
-				'type' => 'url',
-				'section' => 'editing',
-				'hide-if' => [ '!==', 'edit-source', '1' ],
-				'default' => $wikiRequestManager->getExtraFieldData( 'sourceurl' ),
 			]
 		);
 
