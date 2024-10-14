37d9f57be4e7e561f2167f8ae09ea9a64377c230
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-14 16:19:20 -0600

    try

--- includes/HookHandlers/RequestWiki.php
+++ includes/HookHandlers/RequestWiki.php
@@ -594,7 +594,7 @@ class RequestWiki implements
 		$isNsfw = $wikiRequestManager->getExtraFieldData( 'nsfw' );
 		$nsfwMessage = new RawMessage( $isNsfw ?
 			new IconWidget( [ 'icon' => 'check', 'flags' => 'success' ] ) . " '''Yes'''" :
-			new IconWidget( [ 'icon' => 'cancel', 'style' => 'color: #d73333;' ] ) . " '''No'''"
+			new IconWidget( [ 'icon' => 'close', 'style' => 'color: #d73333;' ] ) . " '''No'''"
 		);
 
 		RequestWikiFormUtils::insertFieldAfter(
