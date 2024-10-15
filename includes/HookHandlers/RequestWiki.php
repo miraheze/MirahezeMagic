a309bb1f786d1400a80f8ceeede663dea3d34974
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-15 16:57:00 -0600

    -

--- includes/HookHandlers/RequestWiki.php
+++ includes/HookHandlers/RequestWiki.php
@@ -48,8 +48,8 @@ class RequestWiki implements
 
 	public function onRequestWikiFormDescriptorModify( array &$formDescriptor ): void {
 		$formDescriptor['purpose']['options'] = [ 'Select an option' => '' ] + $formDescriptor['purpose']['options'];
-		$formDescriptor['purpose']['default'] = '';
-		$formDescriptor['purpose']['required'] = true;
+		// $formDescriptor['purpose']['default'] = '';
+		// $formDescriptor['purpose']['required'] = true;
 		RequestWikiFormUtils::addFieldToEnd(
 			$formDescriptor,
 			newKey: 'nsfw',
