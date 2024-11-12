fdb9a9f79625b33c44cab9a7517a5edfaa0b0f57
Agent Isai <43097272+AgentIsai@users.noreply.github.com>
2024-11-12 01:55:34 -0600

    Create getSiteInfo script

---
+++ maintenance/getSiteInfo.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Miraheze\MirahezeMagic\Maintenance;
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+
+require_once "$IP/maintenance/Maintenance.php";
+
+use Maintenance;
+use MediaWiki\MainConfigNames;
+
+class GetSiteInfo extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->addDescription( 'Returns information about a wiki like sitename, URL, etc.' );
+	}
+
+	public function execute() {
+    $language = $this->getConfig()->get( MainConfigNames::LanguageCode );
+    $license = $this->getConfig()->get( MainConfigNames::RightsText );
+    $sitename = $this->getConfig()->get( MainConfigNames::Sitename );
+    $url = $this->getConfig()->get( MainConfigNames::Server );
+
+		$this->output( "Sitename: $sitename\nURL: $url\nLanguage: $language\nLicense: $license\n" );
+  }
+}
+
+$maintClass = GetSiteInfo::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
