b4d09fd43d10b374b741be9687c9ed4a91abcc62
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-09-09 02:22:32 -0600

    Add wikidb to wgLocalDatabases

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -2,9 +2,11 @@
 
 use MediaWiki\MainConfigNames;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\WikiMap\WikiMap;
 use Wikimedia\Rdbms\Database;
 use Wikimedia\Rdbms\DBQueryError;
 
+$wgLocalDatabases = [ 'wikidb' ];
 $wgGlobalUsageDatabase = false;
 $wgHooks['MediaWikiServices'][] = 'wfOnMediaWikiServices';
 
@@ -15,7 +17,6 @@ function wfOnMediaWikiServices( MediaWikiServices $services ): void {
 		}
 
 		$db = wfInitDBConnection( $services );
-		$db->selectDomain( 'wikidb' );
 		if ( !$db->tableExists( 'echo_unread_wikis', __METHOD__ ) ) {
 			$db->sourceFile( MW_INSTALL_PATH . '/extensions/Echo/sql/mysql/tables-sharedtracking-generated.sql' );
 		}
