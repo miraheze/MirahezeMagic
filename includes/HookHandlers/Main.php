dc8a9f3b38726fd70e7cdfd5ad57c73ebe74a24d
90gq29 <lannuser@proton.me>
2025-10-16 14:39:21 +0200

    Add SMW group page message overrides (#608)
    SMW Curator and Editor roles added

--- includes/HookHandlers/Main.php
+++ includes/HookHandlers/Main.php
@@ -120,6 +120,8 @@ class Main implements
 			'grouppage-sysop',
 			'grouppage-upwizcampeditors',
 			'grouppage-user',
+			'grouppage-smwcurator',
+			'grouppage-smweditor',
 			'importdump-help-reason',
 			'importdump-help-target',
 			'importdump-help-upload-file',
