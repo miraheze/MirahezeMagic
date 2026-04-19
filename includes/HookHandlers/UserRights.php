fca2e668096b55cf0c1c38909b3545f93ecac315
pixDeVl <pixDeVl@github.com>
2026-04-19 15:26:01 -0700

    Add editallcustomprotected right and functionality This adds a new user right, 'editallcustomprotected', which through a hook will allow a User with the right to dynamically be given the rights for any custom RestrictionLevel besides the default 4. It also adds an English and QQQ doc message for right-editallcustomprotected. Resolves T14439

---
+++ includes/HookHandlers/UserRights.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Miraheze\MirahezeMagic\HookHandlers;
+
+use MediaWiki\MediaWikiServices;
+use MediaWiki\MainConfigNames;
+use MediaWiki\Permissions\Hook\UserGetRightsHook;
+
+class UserRights implements UserGetRightsHook {
+
+	public static function registerHooks(): void {
+		$hookContainer = MediaWikiServices::getInstance()->getHookContainer();
+		$handler = new self();
+		$hookContainer->register(
+			'UserGetRights',
+			[
+				$handler,
+				'onUserGetRights'
+			]
+		);
+	}
+
+	/** @inheritDoc */
+	public function onUserGetRights( $user, &$rights ) {
+		if ( in_array( 'editallcustomprotected', $rights ) ) {
+			$config = MediaWikiServices::getInstance()->getMainConfig();
+			$levels = $config->get( MainConfigNames::RestrictionLevels );
+			$toAdd = array_diff(
+				$levels,
+				[
+					'',
+					'user',
+					'autoconfirmed',
+					'sysop'
+				]
+			);
+			$rights = array_unique(
+				array_merge(
+					[
+						$rights,
+						$toAdd
+					]
+				)
+			);
+		}
+	}
+}
