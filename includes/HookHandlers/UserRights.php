e625553627232cc931ac01fd0e2a155d08d30987
PixDeVl <pixldevxp@gmail.com>
2026-08-07 21:30:13 -0400

    Add editallcustomprotected right and functionality (#650)
    This adds a new user right, 'editallcustomprotected', which through a
    hook will allow a User with the right to dynamically be given the rights
    for any custom RestrictionLevel besides the default 4. It also adds an
    English and QQQ doc message for right-editallcustomprotected.
    LocalSettings PR: https://github.com/miraheze/mw-config/pull/6485
    Resolves T14439
    
    ---------
    
    Co-authored-by: Skye <pskyechology@gmail.com>

---
+++ includes/HookHandlers/UserRights.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Miraheze\MirahezeMagic\HookHandlers;
+
+use MediaWiki\Config\Config;
+use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
+use MediaWiki\MainConfigNames;
+use MediaWiki\Permissions\Hook\UserGetRightsHook;
+use MediaWiki\Registration\ExtensionRegistry;
+use MediaWiki\User\User;
+
+class UserRights implements UserGetRightsHook {
+
+	public function __construct(
+		private readonly Config $config,
+		private readonly ExtensionRegistry $extensionRegistry
+	) {
+	}
+
+	/**
+	 * @inheritDoc
+	 * @param User $user @phan-unused-param
+	 */
+	public function onUserGetRights( $user, &$rights ): void {
+		// Because tests that don't want to have database access complain
+		if ( defined( 'MW_PHPUNIT_TEST' ) ) {
+			return;
+		}
+
+		if ( $this->extensionRegistry->isLoaded( 'CentralAuth' ) ) {
+			$hasGlobalRight = CentralAuthUser::getInstance( $user )->hasGlobalPermission( 'editallcustomprotected' );
+		} else {
+			$hasGlobalRight = false;
+		}
+		if ( in_array( 'editallcustomprotected', $rights ) || $hasGlobalRight ) {
+			$levels = $this->config->get( MainConfigNames::RestrictionLevels );
+			$toAdd = array_diff(
+				$levels,
+				[
+					'',
+					'user',
+					'autoconfirmed',
+					'sysop'
+				]
+			);
+			$rights = array_unique(
+				array_merge(
+					$rights,
+					$toAdd
+				)
+			);
+		}
+	}
+}
